e90ce7a8465ff3a458cc9acd31ff528eecf13047
& <pair+bmpasini.mediaspork@devbootcamp.com>
2014-07-27 22:58:42 -0400

    stored pet data and time-based pet feeding

--- rubypets.rb
+++ rubypets.rb
@@ -1,9 +1,16 @@
+require 'csv'
+require 'time'
+
 class Pet
-  attr_accessor :name, :hungry, :bored, :sleepy
-  def initialize
+  attr_accessor :name, :birthdate, :status, :hungry, :next_feed_at, :bored, :play_count, :sleepy
+  def initialize(pet_data = {})
     @name = ""
+    @birthdate = Time.now
+    @status = "cranky"
     @hungry = true
+    @next_feed_at = birthdate
     @bored = true
+    @play_count = 0
     @sleepy = true
   end
 end
@@ -20,11 +27,21 @@ class Food
 end
 
 module Caretaker
-  def self.feed(pet)
-    pet.hungry = false
-    new_food = Food.new
-    puts "You fed #{pet.name} a #{new_food.type}."
-    puts "#{pet.name} is no longer hungry."
+  def self.feed(pet, pet_data)
+    if Time.now > Time.parse(pet.next_feed_at.to_s)
+      pet.hungry = false
+      new_food = Food.new
+      puts "You fed #{pet.name} a #{new_food.type}."
+      puts "#{pet.name} is no longer hungry."
+      pet.next_feed_at = (Time.now + 120).to_s
+      pet.status = "happy"
+      current_pet_data = CSV.read(pet_data)
+      File.open(pet_data, "w"){ |csv| csv.print "#{pet.name},#{pet.birthdate},#{pet.status},#{pet.hungry},#{pet.next_feed_at},#{pet.bored},#{pet.play_count},#{pet.sleepy}" }
+    else
+      pet.status = "disturbed"
+      puts "#{pet.name} is not hungry, and gets slightly disturbed!"
+      File.open(pet_data, "w"){ |csv| csv.print "#{pet.name},#{pet.birthdate},#{pet.status},#{pet.hungry},#{pet.next_feed_at},#{pet.bored},#{pet.play_count},#{pet.sleepy}" }
+    end
   end
 
   def self.play(pet)
@@ -64,6 +81,10 @@ class View
     puts "Congratulations! You are the proud parent of #{name}."
   end
 
+  def display_overall_status(pet)
+    puts "#{pet.name} is #{pet.status}."
+  end
+
   def display_food_status(pet)
     if pet.hungry == true
       puts "#{pet.name} is still hungry."
@@ -94,26 +115,48 @@ class View
 end
 
 class Controller
-  attr_reader :view
+  attr_reader :view, :data_file, :csv_pet_data
   attr_accessor :pet
 
   include Caretaker
 
-  def initialize
+  def initialize(pet_data)
     @view = View.new
     @pet = Pet.new
-    view.display_name_request
-    pet.name = view.get_input
-    view.display_birth(pet.name)
-    view.display_food_status(pet)
-    view.display_bored_status(pet)
-    view.display_sleepy_status(pet)
-    view.display_instructions
+    @data_file = pet_data
+    @csv_pet_data = CSV.read(pet_data)
+    if csv_pet_data[0].nil?
+      view.display_name_request
+      pet.name = view.get_input
+      pet.birthdate = Time.now
+      File.open(pet_data, "w"){ |csv| csv.print "#{pet.name},#{pet.birthdate},#{pet.status},#{pet.hungry},#{pet.next_feed_at},#{pet.bored},#{pet.play_count},#{pet.sleepy}" }
+      view.display_birth(pet.name)
+      view.display_food_status(pet)
+      view.display_bored_status(pet)
+      view.display_sleepy_status(pet)
+      view.display_instructions
 
-    run
+      run
+    else
+      pet.name = csv_pet_data[0][0]
+      pet.birthdate = csv_pet_data[0][1]
+      pet.status = csv_pet_data[0][2]
+      pet.hungry = csv_pet_data[0][3]
+      pet.next_feed_at = csv_pet_data[0][4]
+      pet.bored = csv_pet_data[0][5]
+      pet.play_count = csv_pet_data[0][6]
+      pet.sleepy = csv_pet_data[0][7]
+      view.display_food_status(pet)
+      view.display_bored_status(pet)
+      view.display_sleepy_status(pet)
+      view.display_instructions
+
+      run
+    end
   end
 
   def run
+    view.display_overall_status(pet)
     input = ""
     until input == 'exit'
       input = view.get_input.downcase
@@ -125,7 +168,7 @@ class Controller
       when 'play'
         Caretaker.play(pet)
       when 'feed'
-        Caretaker.feed(pet)
+        Caretaker.feed(pet, data_file)
       when 'sleep'
         Caretaker.put_to_bed(pet)
       else
@@ -135,4 +178,4 @@ class Controller
   end
 end
 
-Controller.new
+Controller.new('pet_data.csv')
